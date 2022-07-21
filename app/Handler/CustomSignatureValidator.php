a3bb7e87d4c0954d3d8b66442d74444e0eec71b6
unknown <vikassingh438.com>
2022-07-21 10:49:29 +0530

    webhook callg

--- app/Handler/CustomSignatureValidator.php
+++ app/Handler/CustomSignatureValidator.php
@@ -1,11 +1,8 @@
 <?php
 namespace App\Handler;
-use Illuminate\Http\Request;
-use Spatie\WebhookClient\Exceptions\WebhookFailed;
-use Spatie\WebhookClient\WebhookConfig;
 use Spatie\WebhookClient\SignatureValidator\SignatureValidator;
 
-class ShadowSignature implements SignatureValidator{
+class CustomSignatureValidator implements SignatureValidator{
     public function isValid(Request $request, WebhookConfig $config): bool{
         $signature = $request->header($config->signatureHeaderName);
         if (! $signature) {
