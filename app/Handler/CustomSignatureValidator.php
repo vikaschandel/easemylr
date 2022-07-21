83bcd4fdecba4a58e0e7276d661fab93b8b6a37a
unknown <vikassingh438.com>
2022-07-21 11:39:21 +0530

    webhook call dds

--- app/Handler/CustomSignatureValidator.php
+++ app/Handler/CustomSignatureValidator.php
@@ -7,8 +7,19 @@ use Spatie\WebhookClient\SignatureValidator\SignatureValidator;
 
 class CustomSignatureValidator implements SignatureValidator { 
     
-    public function isValid(): bool {
-        return true;
-      }    
+    public function isValid(Request $request, WebhookConfig $config): bool{
+
+            $signature = $request->header($config->signatureHeaderName);
+            if (! $signature) {
+            return false;
+            }
+            $signingSecret = $config->signingSecret;
+            if (empty($signingSecret)) {
+            throw WebhookFailed::signingSecretNotSet();
+            }
+            $computedSignature = hash_hmac('sha512', $request->getContent(), $signingSecret);
+            return hash_equals($signature, $computedSignature);
+
+        }      
 
 }
