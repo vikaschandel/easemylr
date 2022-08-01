d4b4270c6ff05b0530348383ab2db37cc2963245
vikaschandel <vikassingh438>
2022-08-01 14:38:09 +0530

    lr1

--- resources/views/layouts/includes/navbar.blade.php
+++ resources/views/layouts/includes/navbar.blade.php
@@ -114,9 +114,9 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
                     </a>
                     <ul class="collapse submenu list-unstyled animated fadeInUp" id="forms"  data-parent="#topAccordion">
-                        <li>
+                        <!-- <li>
                             <a href="{{$prefixurl.'orders'}}">Order Booking </a>
-                        </li>
+                        </li> -->
                         <li>
                             <a href="{{$prefixurl.'consignments/create'}}">Create Consignment </a>
                         </li>
